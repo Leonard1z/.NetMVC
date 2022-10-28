40901116605d23a9f1e654d248fc07976935f8ed
Leonard1z <leonardzab0@gmail.com>
2022-10-28 22:47:29 +0200

    #

---
+++ ECommerceWeb/Controllers/ProductController.cs
@@ -0,0 +1,100 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using ECommerce.DataAccess;
+using ECommerce.Models;
+using ECommerce.DataAccess.Repository.IRepository;
+
+namespace ECommerceWeb.Controllers
+{
+
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+            if (id == null || id == 0)
+            {
+                //create product
+                return View(product);
+            }
+            else
+            {
+                //update product
+            }
+
+            return View(product);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated succesfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var coverTypeFromDbFisrt = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+            if (coverTypeFromDbFisrt == null)
+            {
+                return NotFound();
+            }
+            return View(coverTypeFromDbFisrt);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted succesfully";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
