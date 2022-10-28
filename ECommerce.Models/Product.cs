7120dc61b70ffded762f206ebdc329c1f91a8c9c
Leonard1z <leonardzab0@gmail.com>
2022-10-28 19:25:20 +0200

    Product Model

---
+++ ECommerce.Models/Product.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public double ListPrice { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public double Price { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public double Price50 { get; set; }
+        [Required]
+        [Range(1, 10000)]
+        public double Price100 { get; set; }
+        public string ImageUrl { get; set; }
+        [Required]
+        //Foreign Key
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+    }
+}
