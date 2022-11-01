29d152622584d538bf24bd7bb632552274655df6
Leonard1z <leonardzab0@gmail.com>
2022-11-01 19:39:04 +0100

    Home Page

--- ECommerceWeb/Controllers/HomeController.cs
+++ ECommerceWeb/Controllers/HomeController.cs
@@ -1,21 +1,25 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ECommerce.Models;
+using ECommerce.DataAccess.Repository.IRepository;
 
 namespace ECommerceWeb.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+        return View(productList);
     }
 
     public IActionResult Privacy()
